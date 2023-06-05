69106f787f07206e5032b9c52c22bbaff1df9f14
Emmanuel <emmayodayo@gmail.com>
2023-06-05 02:00:27 +0100

    add styling

--- src/containers/Items/Items.js
+++ src/containers/Items/Items.js
@@ -2,23 +2,39 @@ import React from 'react';
 import style from './Items.module.css'
 
 const Items = () => {
+
 	return (
-		<div className={style.ItemsContainer}>
-			<div>
-				<span>Available Items</span>
-				<div>
-					<div className=''>
-						<input type="text" />
+		<div className={style.ItemsSection}>
+			<div className={style.ItemsInputWrapper}>
+				<h3>Available Items</h3>
+				<div className={style.ItemsInputContainer}>
+					<div className={style.ItemsInput}>
+						<input type="text" className={style.Input} />
 					</div>
 
-					<div>
-						<input type="text" />
-						<input type="text" />
+					<div className={style.InputSecondaryWrappers}>
+						<input type="text" className={`${style.InputSecondary}`} />
+						<input type="text" className={`${style.InputSecondary}`} />
+					</div>
+				</div>
+			</div>
+
+			<div className={style.ItemsContainer}>
+				<div className={style.ItemWrapper}>
+					<span>image</span>
+					<div className={style.ItemInfo}>
+						<span>Name</span>
+						<span>Brand</span>
+						<span>Price</span>
 					</div>
 				</div>
 
 				<div>
-					items
+					item
+				</div>
+
+				<div>
+					item
 				</div>
 			</div>
 		</div>
